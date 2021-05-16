abf1039abd89fcf3d00bb871a3f259327a82f7e1
Alan Andrade <alan.andradec@gmail.com>
2021-05-15 20:49:32 -0700

    Display as list or table

--- src/tickers/views.cljs
+++ src/tickers/views.cljs
@@ -2,13 +2,10 @@
   (:require
    [re-frame.core :as re-frame]
    [tickers.subs :as subs]
+   [reagent.core :as r]
    ))
 
-(defn trending-coins []
-  (let [coins (re-frame/subscribe [::subs/trending-coins])]
-    [:div
-     [:h1 "Trending Coins"]
-     [:input {:type "button" :value "Refresh" :on-click #(re-frame/dispatch [:request-trending-coins])}]
+(defn as-table [coins]
      (when-not (empty? @coins)
        [:table
         [:thead
@@ -26,6 +23,40 @@
             ]
            )
          ]])
+  )
+
+(defn as-list [coins]
+  [:ol
+   (for  [coin @coins]
+     [:li {:key (-> coin .-item .-id)}
+      [:img {:src  (-> coin .-item .-small)}]
+      (-> coin .-item .-name)
+      (-> coin .-item .-id)])])
+
+(defn table-or-list [data]
+  (let [display (r/atom "table")]
+    (fn [data]
+      [:div
+       [:select {:name "display-type" :on-change #(reset! display (-> % .-target .-value))}
+        [:option {:value "table"} "Table"]
+        [:option {:value "list"} "List"]
+        ]
+       [:div
+        (cond
+          (= @display "table") (as-table data)
+          (= @display "list") (as-list data))
+        ]
+       ]
+      )
+    )
+  )
+
+(defn trending-coins []
+  (let [coins (re-frame/subscribe [::subs/trending-coins])]
+    [:div
+     [:h1 "Trending Coins"]
+     [:input {:type "button" :value "Refresh" :on-click #(re-frame/dispatch [:request-trending-coins])}]
+     [table-or-list coins]
      ]))
 
 (defn main-panel []
